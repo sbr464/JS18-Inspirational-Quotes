4808caeb447964c041078d54ac231879c4a3c1f0
Stephen Richardson <sbr464@gmail.com>
2015-02-02 21:35:06 -0700

    updates

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(document).on('ready', function() {
   
+
+$('#QuoteInputForm').submit(function(){
+  var author = $('#AuthorInput').val();
+  var quote = $('#QuoteInput').val();
+  $('.output').append('<p class="quoteText">' + quote + '</p><p class="quoteAuthor">' + '-- ' + author + '</p>');
+  event.preventDefault();
+  this.reset();
+});
+
 });
